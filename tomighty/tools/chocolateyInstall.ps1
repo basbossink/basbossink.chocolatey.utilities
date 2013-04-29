fa34fed9a4171c4431a4574fd845f1c276e4b447
Bas Bossink <bas.bossink@gmail.com>
2013-04-30 01:18:10 +0200

    Update wishlist

---
+++ tomighty/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿$packageName = 'tomighty' # arbitrary name for the package, used in messages
+$url = 'http://tomighty.googlecode.com/files/tomighty-0.7.1.exe' # download url
+$url64 = $url # 64bit URL here or just use the same as $url
+$validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
+$targetFullName = Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Definition) tomighty-0.7.1.exe
+try { 
+    Get-ChocolateyWebFile "$packageName" "$targetFullName" "$url" "$url64"
+    $wshell = new-object -comObject WScript.Shell
+    $shortCutPath = Join-Path ([Environment]::GetFolderPath(7)) "tomighty.lnk"
+    $shortCut = $wshell.CreateShortcut($shortCutPath)
+    $shortCut.TargetPath = $targetFullName 
+    $shortCut.Save() 
+  Write-ChocolateySuccess "$packageName"
+} catch {
+  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+  throw 
+}
