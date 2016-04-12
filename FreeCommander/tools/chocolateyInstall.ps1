5b90254865a4244303a54f1cfb39230b8ef4a698
Bas Bossink <bas.bossink@gmail.com>
2016-04-12 10:26:07 +0200

    Remove ues of deprecated chocolatey functions.

--- FreeCommander/tools/chocolateyInstall.ps1
+++ FreeCommander/tools/chocolateyInstall.ps1
@@ -4,11 +4,6 @@ $installerType = 'exe'
 $url = 'http://www.freecommander.com/FreeCommanderXE_setup.zip' 
 $silentArgs = '/VERYSILENT','/NORESTART','/CLOSEAPPLICATIONS','/RESTARTAPPLICATIONS','/SUPPRESSMSGBOXES'
 $validExitCodes = @(0)
-try {
-  Install-ChocolateyZipPackage $packageName $url $toolsDir
-  $fileToInstall = Join-Path $toolsDir "FreeCommanderXE_setup.exe"
-  Install-ChocolateyInstallPackage $packageName $installerType $silentArgs $fileToInstall -validExitCodes @validExitCodes
-} catch {
-  Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
-  throw 
-}
+Install-ChocolateyZipPackage $packageName $url $toolsDir
+$fileToInstall = Join-Path $toolsDir "FreeCommanderXE_setup.exe"
+Install-ChocolateyInstallPackage $packageName $installerType $silentArgs $fileToInstall -validExitCodes @validExitCodes
